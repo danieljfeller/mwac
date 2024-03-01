530eb617dc8ca9dc08be02395ef874240d1683c3
Daniel Feller <danielfeller@Daniels-MacBook-Pro.local>
2024-03-01 17:41:39 -0500

    Deployed 1st version of MWAC Viz app

--- mwac_app.py
+++ mwac_app.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.title('Historical Average Snow Depth - Mt. Washington')
 
-
-
+## load data
 historical_data = pd.read_csv("data/cleaned_hermit_lake_snowdepth.csv")
 historical_averages = pd.read_csv("data/historical_averages.csv")
 
+# change variable name
+historical_averages['historical_depth'] = historical_averages['depth_cm']
+historical_averages = historical_averages.drop(['depth_cm'], axis = 1)
 
-st.subheader('Raw data')
-st.write(historical_averages)
+# isolate winter of interest
+winter_selection = st.selectbox("Select Year", options = set(historical_data.winter))
+selection = historical_data.loc[historical_data.winter==winter_selection, ['day_of_winter', 'depth_cm']]
+df = selection.merge(historical_averages, on = 'day_of_winter', how = 'right')
 
+st.line_chart(df, x="day_of_winter", y=["historical_depth", "depth_cm"])
 
-st.line_chart(historical_averages, x="date", y="depth_cm")
