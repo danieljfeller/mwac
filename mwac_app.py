1ba14af3863dfd07b5cb0cf336b14a695693f327
Daniel Feller <danielfeller@Daniels-MacBook-Pro.local>
2024-03-06 20:35:07 -0500

    Improved the app & added metrics

--- mwac_app.py
+++ mwac_app.py
@@ -1,20 +1,48 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Historical Average Snow Depth - Mt. Washington')
 
-## load data
+
+
+## load & format data
 historical_data = pd.read_csv("data/cleaned_hermit_lake_snowdepth.csv")
 historical_averages = pd.read_csv("data/historical_averages.csv")
-
-# change variable name
 historical_averages['historical_depth'] = historical_averages['depth_cm']
 historical_averages = historical_averages.drop(['depth_cm'], axis = 1)
 
-# isolate winter of interest
-winter_selection = st.selectbox("Select Year", options = set(historical_data.winter))
+# TITLE & INFORMATION
+st.title("Snowdepth at Hermit Lake")
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+csv = convert_df(historical_data)
+
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='hermit_lake_snowdepth.csv',
+    mime='text/csv',
+)
+st.divider()
+
+# METRICS
+today = historical_data.loc[historical_data.winter == '2023-2024',]['day_of_winter'].max()
+
+col1, col2 = st.columns(2)
+col1.metric("Current Snowpack",
+          "55cm", "-67%")
+col2.metric("Total Snowfall", "251cm", "-43%")
+
+# SELECT BOX
+st.divider()
+winter_selection = st.selectbox("Select Year", options = set(historical_data.winter), index = 4)
 selection = historical_data.loc[historical_data.winter==winter_selection, ['day_of_winter', 'depth_cm']]
 df = selection.merge(historical_averages, on = 'day_of_winter', how = 'right')
 
-st.line_chart(df, x="day_of_winter", y=["historical_depth", "depth_cm"])
+
+# LINE CHART
+st.area_chart(df, x="day_of_winter", y=["historical_depth", "depth_cm"])
+
+
 
